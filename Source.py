032e34ddaa8d68b4238c12434ba822b49eaa54c8
Angela.Duran <angelad2000@gmail.com>
2021-02-04 09:41:15 -0800

    Completed Missing Number leetcode (1:40)

--- Source.py
+++ Source.py
@@ -14,3 +14,24 @@ def checkDouble(l):
 
 print(checkDouble(l1))
 '''
+
+# Missing Numbers (Complete), used 	15.5 MB of memory
+
+nums = [3, 0, 1]
+# nums = [1]
+
+def checkMissing(nums):
+    n = len(nums)
+
+    for x in range(len(nums) + 1):
+        if (n in nums):
+            n = n - 1
+        else:
+            # print("OUTPUT: " + str(n))
+            return n
+        # n = n - 1  #maybe need if not accepted
+
+
+print("Output: " + str(checkMissing(nums)))
+
+
