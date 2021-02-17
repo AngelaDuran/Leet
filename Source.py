5fc9f68ffe6409caf1f6a89815c8e72cebbd3874
Angela.Duran <angelad2000@gmail.com>
2021-02-17 09:42:45 -0800

    Leetcode Single Number, works when starting at 1. Need to include negatives and 0 for leetcode

--- Source.py
+++ Source.py
@@ -15,6 +15,7 @@ def checkDouble(l):
 print(checkDouble(l1))
 '''
 
+'''
 # Missing Numbers (Complete), used 	15.5 MB of memory
 
 nums = [3, 0, 1]
@@ -33,5 +34,27 @@ def checkMissing(nums):
 
 
 print("Output: " + str(checkMissing(nums)))
+'''
+
+
+#Single Number
+#works if starting with 1 (need negatives as well)
+#nums = [4,1,2,1,2]
+nums = [1]
+missing = []
+
+for i in range(len(nums)):
+    if i+1 in nums:
+        missing.append(True)
+    else:
+        missing.append(False)
+
+for i in range(len(missing)):
+    if missing[i] == True:
+        result = nums.count(i+1)
+        if(result != 2):
+            print(i+1) #return i+1
+
+
 
 
